72f32be202f0be007ac58d4f6c47dc48ae6d7962
Maciej <sppmacd@pm.me>
2021-11-20 15:03:46 +0100

    Rework command line arguments and add basic support for rendering
    This prints raw video data to stdout to allow tools like ffmpeg
    to encode them into a no-lag video :)

--- src/main.cpp
+++ src/main.cpp
@@ -5,64 +5,158 @@
 #include "MIDIFile.h"
 #include "MIDIPlayer.h"
 
+#include <sstream>
 #include <sys/stat.h>
+
+using namespace std::literals;
+
+std::string_view g_exec_name;
+
+enum class BriefUsage
+{
+    Yes,
+    No
+};
+
+static void print_usage_and_exit(BriefUsage brief_usage = BriefUsage::Yes)
+{
+    std::cerr << "Usage: " << g_exec_name << " [options...] [<realtime <MIDI device>>|<play <MIDI file>>]" << std::endl;
+    if(brief_usage == BriefUsage::No)
+    {
+        std::cerr << "Options:" << std::endl;
+        std::cerr << "    -o    Print render to stdout (may be used for rendering with ffmpeg)" << std::endl;
+    }
+    exit(1);
+}
+
+enum class Mode {
+    Realtime,
+    Play
+};
 
 int main(int argc, char* argv[])
 {
-    bool real_time = false;
+    g_exec_name = argv[0];
     std::unique_ptr<MIDI> midi;
+
+    Mode mode {};
+    bool render_to_stdout = false;
+
     if(argc < 2)
-    {
-        std::cout << "Usage: " << basename(argv[0]) << " [path to MIDI file or MIDI device]" << std::endl;
-        return 1;
-    }
+        print_usage_and_exit(BriefUsage::No);
     else
     {
-        struct stat s;
-        if(stat(argv[1], &s) < 0)
+        for(int i = 1; i < argc; i++)
         {
-            std::cout << "Failed to stat file" << std::endl;
-            return 1;
-        }
-        
-        if(S_ISCHR(s.st_mode))
-        {
-            midi = std::make_unique<MIDIDevice>(argv[1]);
-            real_time = true;
-        }
-        else if(S_ISREG(s.st_mode))
-        {
-            std::ifstream stream(argv[1], std::ios::binary);
-            if(stream.fail())
+            std::string_view arg_sv { argv[i] };
+            if(arg_sv.starts_with('-'))
             {
-                std::cout << "Failed to open file." << std::endl;
-                return 1;
+                std::string_view opt_sv = arg_sv.substr(1);
+                if(opt_sv == "o"sv)
+                    render_to_stdout = true;
+                else
+                    std::cerr << "WARNING: Ignoring invalid option: " << arg_sv << std::endl;
             }
-            auto midi_file = std::make_unique<MIDIFile>(stream);
-            if(!midi_file->is_valid())
+            else
             {
-                std::cout << "Failed to read MIDI" << std::endl;
-                return 1;
-            }    
-            midi_file->dump();
-            real_time = false;
-            midi = std::move(midi_file);
-        }
-        else
-        {
-            std::cout << "Invalid file type (should be regular file or character device)" << std::endl;
-            return 1;
+                if(midi)
+                {
+                    std::cerr << "ERROR: Only one mode may be active at once" << std::endl;
+                    return 1;
+                }
+
+                if(arg_sv == "realtime"sv || arg_sv == "play"sv)
+                {
+                    if(++i >= argc)
+                    {
+                        std::cerr << "ERROR: Expected file name for 'realtime' mode" << std::endl;
+                        return 1;
+                    }
+                    std::string_view filename_sv { argv[i] };
+                    struct stat s;
+                    if(stat(argv[i], &s) < 0)
+                    {
+                        std::cerr << "ERROR: Failed to stat file " << filename_sv << std::endl;
+                        return 1;
+                    }
+                    if(arg_sv == "realtime"sv)
+                    {
+                        if(!S_ISCHR(s.st_mode))
+                        {
+                            std::cerr << "ERROR: Expected character device (e.g /dev/midi3), got " << filename_sv << std::endl;
+                            return 1;
+                        }
+                        midi = std::make_unique<MIDIDevice>(std::string{filename_sv});
+                        mode = Mode::Realtime;
+                    }
+                    else if(arg_sv == "play"sv)
+                    {
+                        if(!S_ISREG(s.st_mode))
+                        {
+                            std::cerr << "ERROR: Expected regular file, got " << filename_sv << std::endl;
+                            return 1;
+                        }
+                        std::ifstream stream(std::string{filename_sv}, std::ios::binary);
+                        if(stream.fail())
+                        {
+                            std::cerr << "Failed to open file." << std::endl;
+                            return 1;
+                        }
+                        auto midi_file = std::make_unique<MIDIFile>(stream);
+                        if(!midi_file->is_valid())
+                        {
+                            std::cerr << "Failed to read MIDI" << std::endl;
+                            return 1;
+                        }    
+                        midi_file->dump();
+                        mode = Mode::Play;
+                        midi = std::move(midi_file);
+                    }
+                }
+                else
+                {
+                    std::cerr << "ERROR: Unknown mode" << std::endl;
+                    print_usage_and_exit();
+                }
+            }
         }
     }
 
     sf::RenderWindow window(sf::VideoMode(500, 500), "MIDI Player");
-    window.setFramerateLimit(60);
+
+    std::unique_ptr<sf::RenderTexture> render_texture = [&]()->std::unique_ptr<sf::RenderTexture> {
+        if(render_to_stdout)
+        {
+            if(isatty(STDOUT_FILENO))
+            {
+                std::cerr << "ERROR: stdout is a terminal, refusing to print binary data" << std::endl;
+                return nullptr;
+            }
+
+            auto texture = std::make_unique<sf::RenderTexture>();
+            // TODO: Support custom resolution/FPS/format/...
+            if(!texture->create(1920, 1080))
+            {
+                std::cerr << "ERROR: Failed to create render texture, ignoring" << std::endl;
+                return nullptr;
+            }
+            std::cerr << "INFO: Rendering to stdout (RGBA24 1920x1080 60fps)" << std::endl;
+            if(mode == Mode::Realtime)
+                std::cerr << "WARNING: Realtime mode is not recommended for rendering, consider recording it to MIDI file first and playing" << std::endl;
+            return texture;
+        }
+        return nullptr;
+    }();
+
+    if(!render_texture)
+        window.setFramerateLimit(60);
 
     sf::Font font;
     font.loadFromFile("res/font.ttf");
 
-    MIDIPlayer player{*midi, real_time ? MIDIPlayer::RealTime::Yes : MIDIPlayer::RealTime::No};
+    MIDIPlayer player{*midi, mode == Mode::Realtime ? MIDIPlayer::RealTime::Yes : MIDIPlayer::RealTime::No};
     sf::Clock fps_clock;
+    sf::Time last_fps_time;
     while(player.playing())
     {
         {
@@ -72,35 +166,53 @@ int main(int argc, char* argv[])
             }
         }
         player.update();
-        window.clear();
 
-        float aspect = static_cast<float>(window.getSize().x) / window.getSize().y;
-        auto view = sf::View{sf::FloatRect(0, -128.f / aspect, 128.f, 128.f / aspect)};
-        window.setView(view);
+        auto render = [&](sf::RenderTarget& target, bool preview) {
+            target.clear();
 
-        if(real_time)
-        {
-            player.ended_notes().clear();
-            midi->for_each_event_backwards([&](Event& event) {
-                event.render(player, window);
-            });
-        }
-        else
-        {
-            player.started_notes().clear();
-            midi->for_each_event([&](Event& event) {
-                event.render(player, window);
-            });
-        }
-        player.render_particles(window);
-        {
-            sf::Vector2f window_size {window.getSize()};
-            window.setView(sf::View({0, 0, window_size.x, window_size.y}));
-            sf::Text text{std::to_string(player.current_tick()) + "  " + std::to_string(1.f / fps_clock.restart().asSeconds()) + " fps", font, 10};
-            text.setPosition(5, 5);
-            window.draw(text);
-        }
+            float aspect = static_cast<float>(target.getSize().x) / target.getSize().y;
+            auto view = sf::View{sf::FloatRect(0, -128.f / aspect, 128.f, 128.f / aspect)};
+            target.setView(view);
+            if(mode == Mode::Realtime)
+            {
+                player.ended_notes().clear();
+                midi->for_each_event_backwards([&](Event& event) {
+                    event.render(player, target);
+                });
+            }
+            else
+            {
+                player.started_notes().clear();
+                midi->for_each_event([&](Event& event) {
+                    event.render(player, target);
+                });
+            }
+            player.render_particles(target);
+            {
+                sf::Vector2f target_size {target.getSize()};
+                target.setView(sf::View({0, 0, target_size.x, target_size.y}));
+                std::ostringstream oss;
+                oss << player.current_tick();
+                if(preview)
+                    oss << "  " + std::to_string(1.f / last_fps_time.asSeconds()) + " fps";
+                sf::Text text{oss.str(), font, 10};
+                text.setPosition(5, 5);
+                target.draw(text);
+            }
+        };
+        render(window, true);
         window.display();
+        if(render_texture)
+        {
+            render(*render_texture, false);
+            render_texture->display();
+            auto image = render_texture->getTexture().copyToImage();
+
+            // NOTE: RGBA only!
+            fwrite(image.getPixelsPtr(), 4, image.getSize().x * image.getSize().y, stdout);
+        }
+
+        last_fps_time = fps_clock.restart();
     }
 
     return 0;
