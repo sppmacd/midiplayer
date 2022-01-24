265ea09fcb50a34366be5f71a981af26c92e802f
Maciej <sppmacd@pm.me>
2022-01-24 15:05:16 +0100

    Make render.sh take a (required) MIDI file argument
    + Make executable

--- render.sh
+++ render.sh
@@ -1,1 +1,1 @@
-build/midi -o play test.mid | ffmpeg -f rawvideo -pix_fmt rgba -s:v 1920x1080 -r 60  -i pipe: -c:v hevc_nvenc output.mp4
+build/midi -o play "$1" | ffmpeg -f rawvideo -pix_fmt rgba -s:v 1920x1080 -r 60  -i pipe: -c:v hevc_nvenc output.mp4
