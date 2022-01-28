644ecac8271402122fb77a1a46977d99ba0c6b75
Maciej <sppmacd@pm.me>
2022-01-28 13:00:06 +0100

    Add error checking & output path arg to render.sh

--- render.sh
+++ render.sh
@@ -1,1 +1,10 @@
-build/midi -o play "$1" | ffmpeg -f rawvideo -pix_fmt rgba -s:v 1920x1080 -r 60  -i pipe: -c:v hevc_nvenc output.mp4
+#!/bin/bash
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Usage: ./render.sh <path to MIDI> <path to output>"
+    exit
+fi
+if [ ! -e "$1" ]; then
+    echo "Input file doesn't exist"
+    exit
+fi
+build/midiplayer -o play "$1" | ffmpeg -f rawvideo -pix_fmt rgba -s:v 1920x1080 -r 60 -i pipe: "$2"
