f1f7be7a75871ee3e251e863f44655c92839dd61
Maciej <sppmacd@pm.me>
2022-02-17 14:55:27 +0100

    Config: Make constructor explicit

--- src/Config/AnimatableProperty.h
+++ src/Config/AnimatableProperty.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <fmt/format.h>
 #include <type_traits>
 namespace Config
 {
@@ -10,7 +11,7 @@ class AnimatableProperty
 public:
     AnimatableProperty() = default;
 
-    AnimatableProperty(T&& t)
+    explicit AnimatableProperty(T&& t)
     : m_current_value(t) {}
 
     void set_value_with_factor(T value, double factor)
@@ -23,7 +24,8 @@ public:
 
     T value() const
     {
-        return m_current_value * (1 - m_factor) + m_next_value * m_factor;
+        auto v = m_current_value * (1 - m_factor) + m_next_value * m_factor;
+        return v;
     }
 
     operator T() const { return value(); }
